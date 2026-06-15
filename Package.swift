c5d6fe15560a75e7de78ea28f9bfa6c2b791dc9c
Ryan Cooperster <vectorlabel@sai.com>
2026-06-15 21:14:55 +0000

    Raise deployment target to macOS 14 to match Homebrew libusb build

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "VectorLabel",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(name: "VectorLabel", targets: ["VectorLabel"]),
     ],
